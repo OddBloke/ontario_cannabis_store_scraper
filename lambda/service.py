d90fc353b803fe58d86180fffd460a544bcbd24c
Daniel Watkins <daniel.watkins@canonical.com>
2018-11-09 22:19:49 -0500

    Include variant price and availability in tweets

--- lambda/service.py
+++ lambda/service.py
@@ -6,13 +6,34 @@ import requests
 import twitter
 
 
-def handler(event, context):
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('ocs_tweeter')
-    response = table.scan()
-    assert len(response['Items']) == 1
-    timestamp = response['Items'][0]['last_timestamp']
+TWEET_PREFIX = 'Newly listed on Ontario Cannabis Store:\n{name} by {brand}'
+TWEET_SUFFIX = '\n#ocs\n{url}'
 
+
+def _format_status(entry, content):
+    return (TWEET_PREFIX + content + TWEET_SUFFIX).format(**entry)
+
+
+def _get_standalone_tweet_content(entry):
+    entry['price'] = float(entry['price'])
+    return _format_status(entry, ' (${price:.2f}, {standalone_availability} left)')
+
+
+def _get_variant_tweet_content(entry):
+    # Determine the relevant variants
+    present_variants = sorted([
+        key.split('_')[0] for key in entry
+        if key.endswith('_price') and entry[key] is not None])
+    content = '\n'
+    for variant in present_variants:
+        availability = entry[variant + '_availability']
+        if availability is None or availability == 0:
+            continue
+        content = content + '{} (${:.2f}, {} left)\n'.format(variant, float(entry[variant + '_price'])/100, entry[variant + '_availability'])
+    return _format_status(entry, content)
+
+
+def handler_for_timestamp(timestamp, debug=False):
     # We're always asking for json because it's the easiest to deal with
     morph_api_url = "https://api.morph.io/OddBloke/ontario_cannabis_store_scraper/data.json"
 
@@ -25,19 +46,38 @@ def handler(event, context):
     })
     print(r.json())
 
-    api = twitter.Api(consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
-                      consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
-                      access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
-                      access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+    statuses = []
     new_timestamp = None
     for entry in r.json():
-        entry['price'] = float(entry['price'])
-        status = 'Newly listed on Ontario Cannabis Store:\n{name} by {brand} (${price:.2f})\n#ocs\n{url}'.format(**entry)
         image = entry.get('image')
         if image is not None and not image.startswith('http'):
             image = 'https:' + image
-        print(api.PostUpdate(status, media=image))
+        if entry['standalone_price'] is not None:
+            status = _get_standalone_tweet_content(entry)
+        else:
+            status = _get_variant_tweet_content(entry)
+            print(status, len(status))
         new_timestamp = entry['timestamp']
+
+    if not debug:
+        api = twitter.Api(
+            consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+            consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+            access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
+            access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+        for (status, image) in statuses:
+            print(api.PostUpdate(status, media=image))
+    return new_timestamp
+
+
+def handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('ocs_tweeter')
+    response = table.scan()
+    assert len(response['Items']) == 1
+    timestamp = response['Items'][0]['last_timestamp']
+
+    new_timestamp = handler_for_timestamp(timestamp)
 
     if new_timestamp is not None:
         table.delete_item(Key={'last_timestamp': timestamp})
@@ -47,3 +87,9 @@ def handler(event, context):
         'statusCode': 200,
         'body': r.text,
     }
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        handler_for_timestamp(sys.argv[1], debug=True)
